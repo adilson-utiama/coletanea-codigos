63b785b4027aaa868642f9127a2d114d1dde6025
Adilson <asu2u@yahoo.com.br>
2016-05-25 22:49:32 -0300

    Incluindo conteudo

--- src/coletanea/codigos/random/NumerosAleatorios.java
+++ src/coletanea/codigos/random/NumerosAleatorios.java
@@ -7,11 +7,17 @@ public class NumerosAleatorios {
 	public static void main(String[] args) {
 		
 		Random r = new Random();
-		int nextInt = r.nextInt(100);
-		
+		int nextInt = r.nextInt(10) + 1;
 		System.out.println(nextInt);
 		
 		double random = Math.random();
 		System.out.println(random);
+		
+		int num = (int) (Math.random() * 100);
+		System.out.println(num);
+		
+		Random rand = new Random();
+		String[] nomes = {"Adilson","Wilson","Edson","Gilson","Adelson","Ilton"};
+		System.out.println(nomes[rand.nextInt(nomes.length - 1)]);
 	}
 }
